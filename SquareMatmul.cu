468fd6b1add76345f91c0d266a0e8adb0a4e30d5
AditMeh <aditmeh123@gmail.com>
2024-06-10 00:13:36 -0400

    stats and matmul

--- SquareMatmul.cu
+++ SquareMatmul.cu
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <iostream>
-#include <thread>
 #include <chrono>
+#include <cmath>
 
-#define BLOCK_SIZE 4
-#define GRID_SIZE 1
+#define BLOCK_SIZE 16
 
 __global__ void matmulkernel(float *d_A, float *d_B, float *result, int WIDTH)
 {
@@ -50,47 +49,56 @@ void setrand(float *arr, int len)
     }
 }
 
-int main(int argc, const char *argv[])
+void invoke_kernel(float *h_A, float *h_B, float *h_C, int size)
 {
-    float h_A[(BLOCK_SIZE * 1) * (BLOCK_SIZE * 1)];       // Row major
-    float h_B[(BLOCK_SIZE * 1) * (BLOCK_SIZE * 1)];       // Row major
-    float h_C[(BLOCK_SIZE * 1) * (BLOCK_SIZE * 1)] = {0}; // Row major
-
-    // Make the first and second vector and put it on the GPU
-    setrand(h_A, BLOCK_SIZE * 1);
-    setrand(h_B, BLOCK_SIZE * 1);
-
-    int PRINT_FLAG = (int)(*argv[1] - '0');
-
-    if (PRINT_FLAG)
-    {
-        printvec(h_A, BLOCK_SIZE * 1);
-        printvec(h_B, BLOCK_SIZE * 1);
-    }
     float *d_A, *d_B, *d_C;
 
-    cudaMalloc((void **)&d_A, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float));
-    cudaMalloc((void **)&d_B, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float));
-    cudaMalloc((void **)&d_C, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float));
+    cudaMalloc((void **)&d_A, size * sizeof(float));
+    cudaMalloc((void **)&d_B, size * sizeof(float));
+    cudaMalloc((void **)&d_C, size * sizeof(float));
 
-    cudaMemcpy(d_A, h_A, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, h_A, size * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 dimBlock((BLOCK_SIZE * 1), (BLOCK_SIZE * 1));
-    dim3 dimGrid(1, 1);
+    int side_length = std::sqrt(size);
+    int grid_side_length = (side_length + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-    matmulkernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, (BLOCK_SIZE * 1));
-    cudaMemcpy(h_C, d_C, (BLOCK_SIZE * 1) * (BLOCK_SIZE * 1) * sizeof(float), cudaMemcpyDeviceToHost);
-        
+    dim3 dimBlock(size, size);
+    dim3 dimGrid(grid_side_length, grid_side_length);
+    printf("%d %d\n", grid_side_length, side_length);
 
-    if (PRINT_FLAG)
-    {
-        printvec(h_C, BLOCK_SIZE * 1);
-    }
+    matmulkernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, side_length);
+    cudaMemcpy(h_C, d_C, size * sizeof(float), cudaMemcpyDeviceToHost);
 
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
+}
 
+int main(int argc, const char *argv[])
+{
+
+    for (int i = 1; i < 100; i++)
+    {
+        int size = (BLOCK_SIZE * i) * (BLOCK_SIZE * i);
+        float h_A[size];
+        float h_B[size];
+        float h_C[size] = {0};
+        int PRINT_FLAG = (int)(*argv[1] - '0');
+
+        // Make the first and second vector and put it on the GPU
+        setrand(h_A, BLOCK_SIZE * i);
+        setrand(h_B, BLOCK_SIZE * i);
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        invoke_kernel(h_A, h_B, h_C, size);
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double, std::milli> duration = end - start;
+
+        std::cout << "Execution time: " << duration.count() << " ms" << std::endl;
+    }
     return 0;
 }
